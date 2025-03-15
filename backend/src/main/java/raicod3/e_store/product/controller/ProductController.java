d662112c6731f22a1a571c97af4efe4bce2ecc4a
Bikal Rai <bikalrai2785@gmail.com>
2025-03-15 14:44:23 +0545

    add method to update, delete and getbyid, test all endpoints with postman

--- backend/src/main/java/raicod3/e_store/product/controller/ProductController.java
+++ backend/src/main/java/raicod3/e_store/product/controller/ProductController.java
@@ -9,12 +9,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
 import raicod3.e_store.product.dto.ProductRequestDTO;
@@ -34,20 +38,17 @@ public class ProductController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<?> createProduct(
-			@Valid @RequestBody ProductRequestDTO productRequestDto,
-			BindingResult bindingResult
-			) {
-		if(bindingResult.hasErrors()) {
-			List<String> errors = bindingResult.getAllErrors().stream()
-					.map(error -> error.getDefaultMessage())
+	public ResponseEntity<?> createProduct(@Valid @RequestBody ProductRequestDTO productRequestDto,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			List<String> errors = bindingResult.getAllErrors().stream().map(error -> error.getDefaultMessage())
 					.collect(Collectors.toList());
-			
+
 			Map<String, Object> response = new HashMap<>();
 			response.put("errors", errors);
 			return ResponseEntity.badRequest().body(response);
 		}
-		
+
 		try {
 			ProductResponseDTO productResponseDTO = productService.createProduct(productRequestDto);
 			return new ResponseEntity<>(productResponseDTO, HttpStatus.CREATED);
@@ -58,18 +59,53 @@ public class ProductController {
 		}
 	}
 
-	@GetMapping("/all")
-	public ResponseEntity<?> getAllProducts(
-			@RequestParam(required = false) String productName,
+	@GetMapping("/products")
+	public ResponseEntity<?> getAllProducts(@RequestParam(required = false) String productName,
 			@RequestParam(defaultValue = "10") int limit) {
 
 		try {
 			List<ProductResponseDTO> products = productService.getAllProdcuts(productName, limit);
 			return new ResponseEntity<>(products, HttpStatus.OK);
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
+	
+	@GetMapping("products/{id}")
+	public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable("id") int productId) {
+		try {
+			ProductResponseDTO productById = productService.getProductById(productId);
+			return new ResponseEntity<>(productById, HttpStatus.OK);
+		} catch(ResponseStatusException e) {
+			return ResponseEntity.status(e.getStatusCode()).body(null);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
 
+	@PutMapping("/update/{id}")
+	public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable("id") int productId,
+			@Valid @RequestBody ProductRequestDTO productRequestDTO) {
+		try {
+			ProductResponseDTO updatedProduct = productService.updateProduct(productId, productRequestDTO);
+			return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+		} catch (ResponseStatusException e) {
+			return ResponseEntity.status(e.getStatusCode()).body(null);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+
+	}
+	
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<String> deleteProduct(@PathVariable("id") int productId) {
+		try {
+			return new ResponseEntity<>(productService.deleteProductById(productId), HttpStatus.OK);
+		}catch (ResponseStatusException e) {
+			return ResponseEntity.status(e.getStatusCode()).body(null);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
--- backend/src/main/java/raicod3/e_store/product/service/ProductService.java
+++ backend/src/main/java/raicod3/e_store/product/service/ProductService.java
@@ -1,9 +1,13 @@
 package raicod3.e_store.product.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import raicod3.e_store.category.model.Category;
 import raicod3.e_store.category.repository.CategoryRepository;
@@ -32,8 +36,8 @@ public class ProductService {
 			// map the new product details from the requestDTO
 			newProduct.setName(productRequestDTO.getName());
 			List<Category> categories = productRequestDTO.getCategories().stream()
-					.map(category -> categoryRepo.findByName(category.getName()))
-					.filter(category -> category != null).collect(Collectors.toList());
+					.map(category -> categoryRepo.findByName(category.getName())).filter(category -> category != null)
+					.collect(Collectors.toList());
 			newProduct.setCategories(categories);
 
 			newProduct.setPrice(productRequestDTO.getPrice());
@@ -52,21 +56,82 @@ public class ProductService {
 			throw new CustomException("error creating new product, " + e.getMessage());
 		}
 	}
-	
+
 	// get all products
 	public List<ProductResponseDTO> getAllProdcuts(String productName, int limit) {
 		try {
 			List<Product> allProducts;
-			
-			if(productName != null && !(productName.isEmpty())) {
+
+			if (productName != null && !(productName.isEmpty())) {
 				allProducts = productRepo.findByNameContainingIgnoreCase(productName);
 			} else {
 				allProducts = productRepo.findAll();
 			}
-			
+
 			return allProducts.stream().limit(limit).map(ProductResponseDTO::new).collect(Collectors.toList());
-		} catch(Exception e) {
+		} catch (Exception e) {
 			throw new RuntimeException("Error retrieving all products, " + e.getMessage());
+		}
+	}
+
+	// get a product using id
+	public ProductResponseDTO getProductById(int productId) {
+		try {
+			Optional<Product> existingProduct = productRepo.findById(productId);
+
+			if (!existingProduct.isPresent()) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with id = " + productId + "not found");
+			}
+			
+			Product product = existingProduct.get();
+			return new ProductResponseDTO(product);
+		} catch (Exception e) {
+			throw new RuntimeException("Error getting product with id: " + productId);
+		}
+	}
+
+	// update product using id
+	public ProductResponseDTO updateProduct(int id, ProductRequestDTO productRequestDTO) {
+		try {
+
+			Optional<Product> existingProduct = productRepo.findById(id);
+
+			if (!existingProduct.isPresent()) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with id = " + id + "not found");
+			}
+			
+			Product product = existingProduct.get();
+			product.setName(productRequestDTO.getName());
+			product.setPrice(productRequestDTO.getPrice());
+			product.setCategories(productRequestDTO.getCategories().stream()
+					.map(category -> categoryRepo.findByName(category.getName())).collect(Collectors.toList()));
+			product.setImageUrls(productRequestDTO.getImageUrls());
+			product.setDescription(productRequestDTO.getDescription());
+			product.setQuantity(productRequestDTO.getQuantity());
+			productRepo.save(product);
+			return new ProductResponseDTO(product);
+
+		} catch (Exception e) {
+			throw new RuntimeException("Error updating product with id: " + id);
+		}
+	}
+	
+	//delete product using id
+	public String deleteProductById(int productId) {
+		try {
+			Optional<Product> existingProduct = productRepo.findById(productId);
+			if(!existingProduct.isPresent()) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with id: " + productId + " not found");
+			}
+			
+			Product product = existingProduct.get();
+			
+			productRepo.deleteById(product.getId());
+			
+			return "Successfully delete product with id: " + productId;
+			
+		}catch (Exception e) {
+			throw new RuntimeException("Error deleting product with id: " + productId);
 		}
 	}
 
